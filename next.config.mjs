6301e7c1f831f2a8f3b3939b2c82362b552e6b59
julianDev <juliandavidnunesfranco@hotmail.es>
2024-10-11 16:32:40 -0500

    Implementa configuracion inicial instala dependencias y se crea la rama develop.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+     
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'marvel-live.freetls.fastly.net',
+                port: "",
+                pathname: '/**',
+               
+            }
+        ]
+    }
+};
 
 export default nextConfig;
